4d438802ab7747e656c8e0e654cae0cf02396aa4
sanghee <wjdtkdgml7352@daum.net>
2023-03-23 14:32:10 +0900

    1. 학급 회장

--- src/ps/hashMap/ClassPresident.java
+++ src/ps/hashMap/ClassPresident.java
@@ -1,5 +1,6 @@
 package ps.hashMap;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -7,10 +8,16 @@ import java.util.Scanner;
 // 투표용지에는 반 학생들이 자기가 선택한 후보의 기호(알파벳)가 쓰여져 있으며
 // 선생님은 그 기호를 발표하고 있습니다.
 public class ClassPresident {
-    public String solution(int n, String str) {
-        String answer="";
-        HashMap<String, Integer> voting = new HashMap<>();
-
+    public char solution(int n, String str) {
+        char answer;
+        HashMap<Character, Integer> vote = new HashMap<>();
+        for (char i = 'A'; i < 'D'; i++) {
+            vote.put(i, 0);
+        }
+        for (int i = 0; i < str.length(); i++) {
+            vote.put(str.charAt(i),vote.get(str.charAt(i))+1);
+        }
+        answer = Collections.max(vote.keySet());
         return answer;
     }
     public static void main(String[] args) {
