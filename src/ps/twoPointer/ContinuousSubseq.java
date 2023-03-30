d7a1a3d1ab9785bc06a8e79bee5e10c58eadad62
sanghee <wjdtkdgml7352@daum.net>
2023-03-31 00:01:58 +0900

    4. 연속 부분수열 - 리팩토링

--- src/ps/twoPointer/ContinuousSubseq.java
+++ src/ps/twoPointer/ContinuousSubseq.java
@@ -1,29 +1,23 @@
 package ps.twoPointer;
 // 수열 N에서 연속부분수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 구하는 프로그램
 // 조건 1.연속 부분 수열
+// 이중 for문으로 풀면 O(N^2)이고, 투 포인터로 풀면 O(N)으로 처리할 수 있다.
 
 import java.util.Scanner;
 
 public class ContinuousSubseq {
     public int solution(int n, int m, int[] arr) {
         int answer = 0;
-        int lt = 0, rt = 0;
-        int sum = 0;
-        while(lt!=n && rt!=n){
-            sum += arr[rt++];
-            System.out.println("lt:"+lt+" rt:"+rt);
-            System.out.println("sum:"+sum);
-            if(sum == m) {
-                answer++; lt++; rt = lt;sum=0;
+        int lt = 0, rt = 0, sum = 0;
+        for (rt = 0; rt < n; rt++) {
+            sum += arr[rt]; // rt가 하나씩 증가
+            if(sum==m) answer++;
+            while(sum>=m) {
+                // lt를 하나 삭제
+                sum-=arr[lt++];
+                if(sum==m) answer++;
             }
-            else if(sum > m) {
-                lt++;
-                rt = lt;
-                sum=0;
-            }
-
         }
-
         return answer;
     }
     public static void main(String[] args) {
