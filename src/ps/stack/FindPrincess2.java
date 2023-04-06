93fadb52479978964a4b5f2852e2bf189689922c
sanghee <wjdtkdgml7352@daum.net>
2023-04-06 11:03:40 +0900

    6. 공주 구하기 - Queue 리팩토링

--- src/ps/stack/FindPrincess2.java
+++ src/ps/stack/FindPrincess2.java
@@ -8,18 +8,13 @@ public class FindPrincess2 {
     public int solution(int n, int k) {
         int answer = 0;
         Queue<Integer> Q = new LinkedList<>();
-        for (int i = 1; i <= n; i++) {
-            Q.offer(i); // 1~n의 값 넣음
-        }
-        while(Q.size() != 1) {
-            for (int i = 0; i < k-1; i++) {
-                int tmp = Q.poll();
-                Q.offer(tmp);
-            }
+        for (int i = 1; i <= n; i++) Q.offer(i); // 1~n의 값 넣음
+        while(!Q.isEmpty()) {
+            for (int i = 1; i < k; i++) Q.offer(Q.poll()); // 꺼낸 값을 뒤에 다시 넣음
             int K = Q.poll();
             System.out.println("내보낼 K번째 왕자 :"+ K);
+            if(Q.size() == 1) answer = Q.poll();
         }
-        answer = Q.element();
         return answer;
     }
     public static void main(String[] args) {
