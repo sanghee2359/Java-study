a79c6c351e48176186a7772991704df3e060209a
sanghee <wjdtkdgml7352@daum.net>
2023-04-06 10:41:09 +0900

    6. 공주 구하기 - stack과 List 사용

--- src/ps/stack/FindPrincess.java
+++ src/ps/stack/FindPrincess.java
@@ -17,25 +17,18 @@ public class FindPrincess {
             prince.add(i); // prince의 번호 넣음
         }
         System.out.println(prince);
-        int idx = 0; // 몇 번째 왕자인지 알려주는 idx변수
+        int idx = 0; // 리스트 안에서 몇 번 왕자인지 알려준다.
         while(n != 1) {
-            if(idx >= n) idx = 0;
+            if(idx >= n) idx = 0; // 리스트 끝에 도달하면 0에서 다시 시작(원형)
             stack.push(prince.get(idx++));
 
-//            for (int i = 0; i < stack.size(); i++) {
-//                System.out.printf(stack.get(i)+" ");
-//            }
-//            System.out.println();
-
-            // i번째 사람이 k를 부르면 탈락
+            // idx번째 사람이 k를 부르면 탈락 & k를 부른 idx번째 사람을 제외하고, 그 다음 사람부터 다시 1부터 시작
             if(stack.size() == k) {
-                prince.remove(idx-1); // arrayList에서 값 삭제
-//                System.out.println("값 하나 삭제"+prince);
+                prince.remove(idx-1); // idx-1번째 사람을 제외(idx를 앞에서 ++ 했으므로)
                 stack.clear(); // 스택 초기화
-                idx = idx-1; // 인덱스 초기화하면 안되지. 다음 인덱스부터 다시 돌아야함.
+                idx = idx-1; // 다음 인덱스부터 다시 순서를 돈다.
                 n--;
             }
-            // i번째 사람을 제외하고, 그 다음 사람부터 다시 1을 시작.
         }
         answer = prince.get(idx);
         return answer;
