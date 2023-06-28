a1e446af1d216e833c3a019d6251e85ab81ae3ba
sanghee <wjdtkdgml7352@daum.net>
2023-06-28 22:16:59 +0900

    10. 봉우리 - 리팩토링

--- src/ps/array/MountainTop.java
+++ src/ps/array/MountainTop.java
@@ -3,38 +3,39 @@ package ps.array;
 import java.util.Arrays;
 import java.util.Scanner;
 // 봉우리 지역이 몇 개 있는지 알아내는 프로그램
-// 각 격자ㅏㄴ의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자 = 봉우리지역
+// 각 격자판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자 = 봉우리지역
 public class MountainTop {
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) {
         MountainTop T = new MountainTop();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] area = new int[n+2][n+2];
-        for (int i = 0; i < n+2; i++) {
-            for (int j = 0; j < n+2; j++) {
-                if(i==0||j==0||i==n+1||j==n+1) area[i][j] = 0;
-                else{area[i][j] = sc.nextInt();}
+        int[][] area = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                area[i][j] = sc.nextInt();
             }
         } // input
-
         System.out.println(T.solution(n, area));
     }
     private int solution(int n, int[][] area) {
         int answer = 0;
-        n = area.length;
-        int[] compare = new int[4];
-        for (int i = 1; i < n-1; i++) {
-            for (int j = 1; j < n-1; j++) {
-                compare[0] = area[i-1][j];
-                compare[1] = area[i+1][j];
-                compare[2] = area[i][j-1];
-                compare[3] = area[i][j+1];
-                Arrays.sort(compare);
-                if(compare[3] < area[i][j]) answer++;
-                else continue;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i+dx[k];
+                    int ny = j+dy[k];
+                    if(nx >= 0 && nx < n && ny >=0 && ny < n &&
+                            area[nx][ny] >= area[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) answer++;
             }
         }
-
         return answer;
     }
 }
