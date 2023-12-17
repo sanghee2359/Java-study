4d9bab0d15491d34b0d93e42c7a06c144aaf8611
wjdtk <wjdtkdgml7352@naver.com>
2023-12-18 00:21:48 +0900

    1.문자 찾기

--- src/ps/string/FindLetter.java
+++ src/ps/string/FindLetter.java
@@ -1,26 +1,26 @@
 package ps.string;
-// 특정 문자가 문자열에 몇 개 존재하는지 알아내는 프로그램
 
 import java.util.Scanner;
 
-public class FindLetter {
-    public int solution(String str, char c) {
-        int answer = 0;
-        // lowercase
-        str = str.toLowerCase();
-        c = Character.toLowerCase(c);
 
-        // 값 비교
-        for(char x : str.toCharArray()){
-            if(x==c) answer++;
+public class FindLetter {
+    public int Find(String str, char let) {
+        int answer = 0;
+        str = str.toUpperCase();
+        let = Character.toUpperCase(let);
+        for (int i = 0; i < str.length(); i++) {
+            if(let == str.charAt(i)) answer++;
         }
+//        System.out.println(answer);
         return answer;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        FindLetter T = new FindLetter();
-        String str = sc.nextLine();
-        char c = sc.next().charAt(0);
-        System.out.println(T.solution(str, c));
+    public static void main(String[] args){
+        FindLetter O = new FindLetter();
+        Scanner in=new Scanner(System.in);
+        String str = in.next();
+        char let = in.next().charAt(0);
+
+        System.out.println(O.Find(str,let));
+        return ;
     }
 }
