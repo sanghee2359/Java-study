8340842d41209fa94ac5d177113287b5d48e3a87
wjdtk <wjdtkdgml7352@naver.com>
2023-12-25 18:12:35 +0900

    4. 재귀함수 - 피보나치 수열 (D(N)을 static 배열에 기록)

--- src/ps/recursive/Fibonacci.java
+++ src/ps/recursive/Fibonacci.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 // 피보나치 수열 : 앞의 2개의 수를 합하여 다음 숫자가 되는 수열
 public class Fibonacci {
+    static int[] fibo;
     public int DFS(int num) {
-        if(num == 1) return 1;
-        else if(num == 2) return 1;
+        if(num == 1) return fibo[1] = 1;
+        else if(num == 2) return fibo[2] = 1;
         else {
-            return DFS(num-1)+DFS(num-2);
+            return fibo[num] = DFS(num-1)+DFS(num-2);
         }
 
     }
@@ -16,8 +17,10 @@ public class Fibonacci {
         Fibonacci O = new Fibonacci();
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
+        fibo = new int[num+1];
+        O.DFS(num);
         for (int i = 1; i <= num; i++) {
-            System.out.printf(O.DFS(i)+ " ");
+            System.out.printf(fibo[i]+ " ");
         }
     }
 }
