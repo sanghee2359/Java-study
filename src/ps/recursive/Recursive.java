1c924b770cc65032280d9110af4d091b3ceaf8c3
wjdtk <wjdtkdgml7352@naver.com>
2023-12-25 17:03:17 +0900

    1. 재귀함수 - 스택 사용

--- src/ps/recursive/Recursive.java
+++ src/ps/recursive/Recursive.java
@@ -1,11 +1,12 @@
 package ps.recursive;
 // 1부터 n까지 출력하는 프로그램
 public class Recursive {
+    // 스택 프레임 사용 - 매개변수, 지역변수 ,복귀주소 정보가 저장
     public void DFS(int n) {
         if(n == 0) return;
         else {
             DFS(n-1);
-            System.out.println(n);
+            System.out.print(n+" ");
         }
     }
     public static void main(String[] args) {
