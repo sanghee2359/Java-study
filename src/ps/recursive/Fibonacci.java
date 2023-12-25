67a772943f8afa7aafdebb8694410d121bc0716d
wjdtk <wjdtkdgml7352@naver.com>
2023-12-25 18:04:31 +0900

    4. 재귀함수 - 피보나치 수열 D(1) ~ D(10) 출력

--- src/ps/recursive/Fibonacci.java
+++ src/ps/recursive/Fibonacci.java
@@ -4,13 +4,11 @@ import java.util.Scanner;
 
 // 피보나치 수열 : 앞의 2개의 수를 합하여 다음 숫자가 되는 수열
 public class Fibonacci {
-
     public int DFS(int num) {
         if(num == 1) return 1;
         else if(num == 2) return 1;
         else {
             return DFS(num-1)+DFS(num-2);
-//            arr[num] = arr[num-1]+arr[num-2];
         }
 
     }
@@ -18,6 +16,8 @@ public class Fibonacci {
         Fibonacci O = new Fibonacci();
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
-        System.out.println(O.DFS(num));
+        for (int i = 1; i <= num; i++) {
+            System.out.printf(O.DFS(i)+ " ");
+        }
     }
 }
