964f08fc4b36719648e564a1644ba40f05971c16
wjdtk <wjdtkdgml7352@naver.com>
2023-12-30 23:40:42 +0900

    14. 중복 순열 구하기 - 리팩토링

--- src/ps/dfs/PermutationRepetition.java
+++ src/ps/dfs/PermutationRepetition.java
@@ -3,21 +3,20 @@ package ps.dfs;
 import java.util.Scanner;
 // 중복 순열 구하기 : 1~N 번호가 적힌 구슬이 있는데, 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열하는 방법을 모두 출력한다
 public class PermutationRepetition {
-//    static int[][] arr;
-    static int [] arr;
     static int n,m;
-    public void DFS(int L, int M) {
-        if(M == m) {
-            System.out.printf(L+" ");
-            for (int i = 0; i <= arr.length; i++) {
-                if(arr[i] == 1) System.out.println(i);
+    static int [] arr;
+    public void DFS(int L) {
+        if(L == m) {
+            for (int x: arr) {
+                System.out.printf(x+" ");
             }
+            System.out.println();
         }
         else{
             for (int i = 1; i <= n; i++) {
-                arr[i] = 1;
-                DFS(i, M+1);
-                arr[i] = 0;
+                arr[L] = i;
+                DFS(L+1);
+                arr[L] = 0;
             }
         }
     }
@@ -27,12 +26,15 @@ public class PermutationRepetition {
         // 구술의 수
         n = in.nextInt();
         // 중복을 허락하여 m번을 뽑아 일렬로 나열하는 방법 출력
-        int m = in.nextInt();
-        arr = new int[n+1];
-        O.DFS(0,1);
+        m = in.nextInt();
+        arr = new int[m];
+        O.DFS(0);
 //        for (int i = 0; i < arr.length; i++) {
 //            System.out.println();arr[i][0] = in.nextInt();
 //            arr[i][1] = in.nextInt();
+
+
+
 //        }
     }
 }
