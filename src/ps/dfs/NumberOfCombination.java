46a02c2f9d0b2c671b606cc1d910d612d81ce47a
wjdtk <wjdtkdgml7352@naver.com>
2023-12-31 11:57:33 +0900

    17. 조합수 구하기

---
+++ src/ps/dfs/NumberOfCombination.java
@@ -0,0 +1,23 @@
+package ps.dfs;
+
+import java.util.Scanner;
+
+// 조합의 경우의 수
+// 조합 = 순서를 고려하지 않고 뽑은 경우의 수 (숫자가 중복되지 않으면서 순서와 관계없다)
+// nCr = n-1 C r-1 + n-1 C r 공식을 사용하여 조합수를 구하는 프로그램
+public class NumberOfCombination {
+    public int DFS(int n, int r){
+        if(n == r || r == 0){
+            return 1;
+        }else {
+            return DFS(n-1, r-1)+DFS(n-1, r);
+        }
+    }
+    public static void main(String[] args) {
+        NumberOfCombination O = new NumberOfCombination();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int r = in.nextInt();
+        System.out.println(O.DFS(n, r));
+    }
+}
