5214d2bf5eedddff2558426e53d9a8e69c562029
wjdtk <wjdtkdgml7352@naver.com>
2024-01-02 23:53:57 +0900

    18. 수열 추측하기

---
+++ src/ps/dfs/GuessingTheSequence.java
@@ -0,0 +1,14 @@
+package ps.dfs;
+
+import java.util.Scanner;
+
+public class GuessingTheSequence {
+    public void DFS(int n, int m) {}
+    public static void main(String[] args) {
+        GuessingTheSequence O = new GuessingTheSequence();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        O.DFS(n, m);
+    }
+}
