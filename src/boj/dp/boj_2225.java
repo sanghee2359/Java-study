7a0a6a40be16b7c787687f7e9891eb2eaaceda09
wjdtk <wjdtkdgml7352@naver.com>
2024-07-15 23:57:16 +0900

    add : boj_2225_합분해 Solved

--- src/boj/dp/boj_2225.java
+++ src/boj/dp/boj_2225.java
@@ -7,7 +7,17 @@ public class boj_2225 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); // 1 <= N <= 200
         int K = sc.nextInt(); // 1 <= K <= 200
+        int[][] dp = new int[201][201];
 
-
+        for(int i=1;i<=K;i++) {
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=K;i++) {
+            for(int j=1;j<=N;j++) {
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j])%1000000000; // 1000000000으로 나누는 걸 출력할 때 주면 틀렸다고 뜸.
+            }
+        }
+        System.out.println(dp[K][N]);
     }
+
 }
