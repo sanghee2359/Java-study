312532961cbb57960723f96fc5d9a70f9ab75107
wjdtk <wjdtkdgml7352@naver.com>
2024-08-01 15:18:18 +0900

    add : boj_10989_수 정렬하기3 Solved
     Counting 정렬으로 풀이 - 시간복잡도 O(N)를 가진다

--- src/boj/sort/boj_10989.java
+++ src/boj/sort/boj_10989.java
@@ -3,21 +3,40 @@ package boj.sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+
 
 // O(NlogN) 보다 빨라야 한다.
 public class boj_10989 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int[] array = new int[N];
+        int[] result = new int[N];
+        int max = 0;
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
-            int tmp = Integer.parseInt(br.readLine());
-            heap.add(tmp);
+            array[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, array[i]);
+        }
+        int[] counting = new int[max+1];
+
+        for (int i = 0; i < N; i++) {
+             counting[array[i]]++;
+        }
+        for (int i = 1; i <= max; i++) {
+            counting[i] += counting[i-1];
+        }
+
+        for (int i = N-1; i >= 0; i--) {
+            int value = array[i];
+            counting[value]--;
+            result[counting[value]] = value;
         }
         for (int i = 0; i < N; i++) {
-            System.out.println(heap.poll());
+            sb.append(result[i]).append("\n");
         }
+        System.out.println(sb);
     }
 }
