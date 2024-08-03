f38c1b848248b80001694e9e71ef66a2e0998cec
wjdtk <wjdtkdgml7352@naver.com>
2024-08-03 13:43:04 +0900

    add : String - 4. 단어 뒤집기

---
+++ src/inflearn/string/FlippingWord.java
@@ -0,0 +1,21 @@
+package inflearn.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FlippingWord {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+        for (int i = 0; i < N; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(arr[i]);
+            System.out.println(sb.reverse());
+        }
+    }
+}
