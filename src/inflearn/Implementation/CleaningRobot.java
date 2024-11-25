f8a2568489d281e034446a142ec46a72929b5cff
wjdtk <wjdtkdgml7352@naver.com>
2024-11-25 20:40:50 +0900

    add : 구현 - 청소 로봇 (리팩토링)

--- src/inflearn/Implementation/CleaningRobot.java
+++ src/inflearn/Implementation/CleaningRobot.java
@@ -1,31 +1,25 @@
 package inflearn.Implementation;
 import java.util.*;
 public class CleaningRobot {
-    static int[] dx = {1, 0, -1, 0}, dy = {0, -1, 0, 1};
     public int[] solution(int[][] board, int k){
         int[] answer = new int[2];
         int N = board.length; // 5
-        int x = 0; // 0
-        int y = 0;
-        int idx = 0;
-        int ny = y + dy[idx]; // y가 x축
-        int nx = x + dx[idx];
-        while(k-->0) {
-            if(idx < 4) {
-                ny = y + dy[idx]; // y가 x축
-                nx = x + dx[idx]; // x가 y축
-                if(nx >= N || ny >= N
-                        || nx < 0  || ny < 0
-                        ||board[ny][nx] == 1) {
-                    idx++;
-                    continue;
-                }
-                x = nx;
-                y = ny;
-            }else idx = 0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int x = 0, y = 0, idx = 1, count = 0;
+        while(count < k) {
+            count++;
+            int nx = x + dx[idx];
+            int ny = y + dy[idx];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[nx][ny] == 1) {
+                idx = (idx + 1) % 4;
+                continue;
+            }
+            x = nx;
+            y = ny;
         }
-        answer[0] = ny;
-        answer[1] = nx;
+        answer[0] = x;
+        answer[1] = y;
         return answer;
     }
 
