68d19c704b94ee1f9f7b766de45811f21682be08
wjdtk <wjdtkdgml7352@naver.com>
2024-12-02 06:53:53 +0900

    add : 구현 - 좌석 번호 (연구)

---
+++ src/inflearn/Implementation/SeatNumber.java
@@ -0,0 +1,48 @@
+package inflearn.Implementation;
+import java.util.*;
+public class SeatNumber {
+    public int[] solution(int c, int r, int k){ // c * r = 강연장 크기, k번 째에 온 사람이 착석할 위치
+        int[] answer = {0, 0};
+        boolean [][] visited = new boolean[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                visited[i][j] = false;
+            }
+        }
+        // 시작 위치 : 제일 왼쪽 & 제일 아래
+        // x좌표? r-1, y좌표 0
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int x = 0, y = 0, d = 0, count = 0;
+        while(count <= c * r) {
+            if(count == k) {
+                break;
+            }
+            visited[x][y] = true;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(nx < 0 || nx >= r || ny < 0 || ny >= c
+                    || visited[nx][ny] ) {
+                d = (d + 1) % 4;
+                continue;
+            }
+            count++;
+            x = nx;
+            y = ny;
+        }
+        if(count > k) {
+            return answer;
+        }
+        answer[0] = x+1;
+        answer[1] = y+1;
+        return answer;
+    }
+
+    public static void main(String[] args){
+        SeatNumber T = new SeatNumber();
+        System.out.println(Arrays.toString(T.solution(6, 5, 12)));
+        System.out.println(Arrays.toString(T.solution(6, 5, 20)));
+//        System.out.println(Arrays.toString(T.solution(6, 5, 30)));
+//        System.out.println(Arrays.toString(T.solution(6, 5, 31)));
+    }
+}
