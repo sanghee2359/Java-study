449cd5e04ff13f313d3fca64ac28956e283cf069
wjdtk <wjdtkdgml7352@naver.com>
2024-12-08 11:27:46 +0900

    add : boj_28702_FizzBuzz (리팩토링)

--- src/boj/math/boj_28702.java
+++ src/boj/math/boj_28702.java
@@ -3,31 +3,28 @@ package boj.math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class boj_28702 {
     public boolean isNumber(String str) {
         return str.chars().allMatch(Character::isDigit);
     }
-    // 3의 배수 & 5의 배수
-    public String solution(String[] strings)
-    {
-        int[] numbers = new int[4];
-        int idx = 0, cur;
-        for (int i = 0; i < strings.length; i++) {
-            if(isNumber(strings[i])) {
-                cur = Integer.parseInt(strings[i]);
-                numbers[i] = cur;
-                idx = i + 1;
-                break;
+    public String solution(String[] strings) {
+        String answer = "";
+        Set<String> set = new HashSet<>(Arrays.asList("Fizz", "Buzz", "FizzBuzz"));
+        int n = 0;
+        for (int i = 0; i < 3; i++) {
+            if(!set.contains(strings[i])) {
+                n = Integer.parseInt(strings[i]) + 3 - i;
             }
         }
-        for (int i = idx; i < 4; i++) {
-            numbers[i] = numbers[i-1] + 1;
-        }
-        if(numbers[3] % 3 == 0 && numbers[3] % 5 == 0) return "FizzBuzz";
-        else if(numbers[3] % 3 == 0 && numbers[3] % 5 != 0) return "Fizz";
-        else if(numbers[3] % 3 != 0 && numbers[3] % 5 == 0) return "Buzz";
-        else return Integer.toString(numbers[3]);
+        if(n % 15 == 0) answer = "FizzBuzz";
+        else if( n % 3 == 0) answer = "Fizz";
+        else if( n % 5 == 0) answer = "Buzz";
+        else answer = String.valueOf(n);
+        return answer;
 
     }
 
