782c94ca3bf47ebc28a552ff5f9a15a1900852ab
wjdtk <wjdtkdgml7352@naver.com>
2024-12-19 06:10:05 +0900

    add : [Silver IV] boj_2839_설탕 배달 (구현)

--- algorithm/src/boj/dp/boj_2839.java
+++ algorithm/src/boj/dp/boj_2839.java
@@ -1,15 +1,20 @@
 package boj.dp;
 
+import java.util.Scanner;
+
 public class boj_2839 {
+    private int solution(int n) {
+        int answer = 0;
+        if(n == 4 || n == 7) answer = -1;
+        else if(n % 5 == 0) answer = n / 5;
+        else if(n % 5 == 1 || n % 5 == 3) answer = (n / 5) + 1;
+        else if(n % 5 == 2 || n % 5 == 4) answer = (n / 5) + 2;
+        return answer;
+    }
     public static void main(String[] args) {
         boj_2839 T = new boj_2839();
-        int[][] dp = new int[5000][11];
-        dp[0][3] = 1;
-        dp[0][5] = 1;
-        for (int i = 1; i < 11; i++) {
-            if(i % dp[0][3] == 0) dp[0][i] = i/dp[0][3];
-            else if(i % dp[0][5] == 0) dp[0][i] = i/dp[0][5];
-            else if(i == 8)  dp[0][i] = dp[0][3]+dp[0][5];
-        }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(T.solution(n));
     }
 }
