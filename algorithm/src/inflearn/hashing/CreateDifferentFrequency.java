c332f588ca663c50e3ad9d059ec0b7ee6c1d0d16
wjdtk <wjdtkdgml7352@naver.com>
2024-12-20 17:09:12 +0900

    add : 해싱 - 서로 다른 빈도수 만들기 (결과 확인)

--- algorithm/src/inflearn/hashing/CreateDifferentFrequency.java
+++ algorithm/src/inflearn/hashing/CreateDifferentFrequency.java
@@ -1,29 +1,27 @@
 package inflearn.hashing;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CreateDifferentFrequency {
     private  int solution(String s) {
         // 문자열의 숫자를 지워, 모든 문자의 빈도수가 서로 다르게 만드려 한다
         // 최소로 문자를 지워서 서로다른 문자열 빈도수가 되도록 하는, 지워야 할 최소개수를 반환하시요
         HashMap<Character, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         for(char x : s.toCharArray()) {
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
-        // 값이 서로 다르도록 만드는 법
-        // 일단 값이 작을 수록 지우면 더 편해보인다.
-        // 그래도 우선 조건은, 같은 entry 중에 겹치는 값이 있을 것.
-
         int count = 0;
-        // 값이 겹치는 게 있는지 어케확인함?
+        // 값이 겹치는 게 있는지 어케확인함? -> set을 통해 겹치는 값이 있는지 비교
         for(char key : map.keySet()) {
-            int tmpValue = map.get(key); // 값 출력
-            // contains에서 자기자신을 제외해서
-            // 현재 값과 같은 값이 있는가? 를 체크 해야 하는데, 자기 자신까지 포함하는 바람에 계속 -1 되고 있다
-            while(map.containsValue(tmpValue)){
+            while(set.contains(map.get(key))) { //set안의 값과 겹치는 값이 없을 때까지 1씩 감소
                 map.put(key, map.getOrDefault(key, 0) - 1);
                 count++;
             }
+            // 값이 존재하지 않을 경우 -> set에 저장
+            if(map.get(key) > 0) set.add(map.get(key));
         }
         return count;
     }
