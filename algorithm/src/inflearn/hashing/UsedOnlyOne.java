ec7572ab37e6a0e7c45e8c1cbdb1b784e572d586
wjdtk <wjdtkdgml7352@naver.com>
2024-12-20 15:47:53 +0900

    add : 해싱 - 한 번 사용한 최초 문자 (리팩토링)

--- algorithm/src/inflearn/hashing/UsedOnlyOne.java
+++ algorithm/src/inflearn/hashing/UsedOnlyOne.java
@@ -4,24 +4,14 @@ import java.util.HashMap;
 
 public class UsedOnlyOne {
     public int solution(String s){
-        int answer = 0;
-        HashMap<Character, int[]> charMap = new HashMap<>();
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        for(char x : s.toCharArray()){
+            charMap.put(x, charMap.getOrDefault(x, 0) + 1);
+        }
         for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
-            if(!charMap.containsKey(cur)) charMap.put(cur, new int[]{i + 1, 1});
-            else { // 포함되어 있을 경우
-                int num = charMap.get(cur)[1] + 1;
-                charMap.put(cur, new int[]{i+1, num});
-            }
+            if(charMap.get(s.charAt(i)) == 1) return i+1;
         }
-        int min = Integer.MAX_VALUE;
-        for (Character key : charMap.keySet()) {
-            if(charMap.get(key)[1] == 1 && min > charMap.get(key)[0]){
-                min = charMap.get(key)[0];
-            }
-        }
-        answer = (min == Integer.MAX_VALUE)? -1 : min;
-        return answer;
+        return -1;
     }
 
     public static void main(String[] args){
