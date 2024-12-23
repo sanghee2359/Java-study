0032370aa739ce6fb5eebb59163d870756da038c
wjdtk <wjdtkdgml7352@naver.com>
2024-12-23 18:19:17 +0900

    add : [Silver IV] boj_10773_제로 (리팩토링)

--- algorithm/src/boj/stack/boj_10773.java
+++ algorithm/src/boj/stack/boj_10773.java
@@ -1,23 +1,31 @@
 package boj.stack;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class boj_10773 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 재현이는 잘못된 수를 부를 때마다 0을 외쳐서, 가장 최근에 재민이가 쓴 수를 지우게 시킨다.
-        Scanner sc = new Scanner(System.in);
-        Stack<Integer> stack = new Stack<>();
-        int n = sc.nextInt(), sum = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()), sum = 0;
+        int[] stack = new int[n];
+        int top = -1;
         for (int i = 0; i < n; i++) {
-            int tmp = sc.nextInt();
-            if(tmp == 0 && !stack.isEmpty()) stack.pop();
-            else stack.push(tmp);
+            int tmp = Integer.parseInt(br.readLine());
+            if(tmp == 0 ){
+                stack[top] = 0;
+                top--;
+            }
+            else {
+                top++;
+                stack[top] = tmp;
+            }
         }
-        int size = stack.size();
-        for (int i = 0; i < size; i++) {
-            sum += stack.pop();
+        for (int i = 0; i < stack.length; i++) {
+            sum += stack[i];
         }
         System.out.println(sum);
+        br.close();
     }
 }
