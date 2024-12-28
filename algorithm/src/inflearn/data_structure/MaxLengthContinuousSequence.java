640b9cfc53cee6f679ff8dff367e3c235ab6d6fd
wjdtk <wjdtkdgml7352@naver.com>
2024-12-28 12:53:11 +0900

    add : 자료구조 - 최대 길이 연속수열 (결과 확인)

--- algorithm/src/inflearn/data_structure/MaxLengthContinuousSequence.java
+++ algorithm/src/inflearn/data_structure/MaxLengthContinuousSequence.java
@@ -1,9 +1,29 @@
 package inflearn.data_structure;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MaxLengthContinuousSequence {
+    // 1씩 증가하는 연속된 수열의 최대 길이
     public int solution(int[] nums){
         int answer = 0;
-
+        ArrayList<Integer> set = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) continue;
+            set.add(nums[i]);
+        }
+        Collections.sort(set);  // 정렬
+        int sum = 1;            // 연속 수열의 길이 합
+        for (int i = 0; i < set.size() - 1; i++) {
+            if(set.get(i) + 1 == set.get(i+1)) {
+                sum++;
+            }
+            else {
+                answer = Math.max(sum, answer);
+                sum = 1;
+            }
+        }
+        answer = Math.max(sum, answer);
         return answer;
     }
 
