403c483add5bd82b5c9a94852cf966df0b2f92ba
wjdtk <wjdtkdgml7352@naver.com>
2025-01-05 17:13:52 +0900

    add : 자료구조 - 가장 많이 사용된 회의실 (리팩토링)

--- algorithm/src/inflearn/data_structure/MostUsedConferenceRoom.java
+++ algorithm/src/inflearn/data_structure/MostUsedConferenceRoom.java
@@ -1,61 +1,40 @@
 package inflearn.data_structure;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
 
 public class MostUsedConferenceRoom {
     public int solution(int n, int[][] meetings) {
         int max = 0, idx = 0;
-        int answer = 0, num = meetings.length, endTime = 0, roomNum = 0;
         int[] result = new int[n];
         TreeSet<Integer> rooms = new TreeSet<>();
-        LinkedList<int[]> meetingSort = new LinkedList<>();
-        // 일찍 끝나는 회의가 가장 먼저 오면서, 시간이 같다면 더 작은 번호를 가진 회의실부터 우선 정렬
         PriorityQueue<int[]> usingRoom = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         for (int i = 0; i < n; i++) {
             rooms.add(i); // 회의실 방 번호 오름차순 정렬
         }
-        for (int i = 0; i < num; i++) {
-            meetingSort.add(new int[]{meetings[i][0], meetings[i][1]}); // 도착 시간, 회의 시간
-        }
-        meetingSort.sort((a, b) -> a[0] - b[0]); // 도착 시간을 기준으로 정렬
-
-
-        while (!meetingSort.isEmpty() || !usingRoom.isEmpty()) {
-            // 비어있는 회의실이 있고, 진행해야할 회의가 남았을 경우
-            if (!rooms.isEmpty() && !meetingSort.isEmpty()) {
-                roomNum = rooms.pollFirst();
-                int[] now = meetingSort.pollFirst();
-                endTime = now[1]; // 끝나는 시간
-                usingRoom.add(new int[]{endTime, roomNum});
-            }else if(rooms.isEmpty() && !usingRoom.isEmpty()){
-                // rooms에 남은 회의실이 없는 경우
-                // 회의실을 쓰는 시간은 (끝난시간 - 도착시간) 즉, 차액만큼 쓴다
-                usingRoom.peek()[0]; // 가장먼저 끝나는 회의가 끝날때롤 점프
-                roomNum = usingRoom.peek()[1]; // roomNum 갱신
-                int[] now = meetingSort.pollFirst(); // 다음 회의 진행할 것
-                int time = now[1] - now[0];
-                endTime = endTime + time;
-                usingRoom.add(new int[] {endTime, roomNum});
+        Arrays.sort(meetings, (a, b)-> a[0] - b[0]); // 도착시간 기준으로 정렬
+        for(int[] m : meetings) {
+            // 현재 도착시간 >= 회의실 이용종료 시간 -> 회의실 비었다
+            while(!usingRoom.isEmpty() && usingRoom.peek()[0] <= m[0]) rooms.add(usingRoom.poll()[1]); // 회의실 방 번호
+            if(!rooms.isEmpty()) { // 회의실이 사용 가능하다면
+                int room = rooms.pollFirst();
+                result[room]++;
+                usingRoom.add(new int[]{m[1], room}); // 이용 종료 시간, 방번호
+            } else { // 회의실 사용이 만석이라면
+                int[] endMeeting = usingRoom.poll(); // 가장 먼저 끝날 회의 꺼내오기
+                result[endMeeting[1]]++;
+                usingRoom.add(new int[]{endMeeting[0] + (m[1] - m[0]), endMeeting[1]});
             }
-            // 도착하는 시간보다 현재 방 사용이 끝나는 시간이 더 작거나 같다면?
-            // room에 사용했던 번호 추가,
-            // result에 room number +1 업데이트
-            while (!meetingSort.isEmpty()
-                    && usingRoom.peek()[0] <= meetingSort.peek()[0]) {
-                int[] endMeeting = usingRoom.poll();
-                int endRoomNum = endMeeting[1];
-                rooms.add(endRoomNum); // 사용한 방번호를 추가
-                result[endRoomNum]++;
-            }
-
         }
+
         for (int i = 0; i < n; i++) {
             if(max < result[i]){
                 max = result[i];
                 idx = i;
             }
         }
-        return answer = idx;
+        return idx;
     }
 
     public static void main(String[] args){
