b8d6768805a436e48b8db43aed20411fd3e4043b
wjdtk <wjdtkdgml7352@naver.com>
2025-01-05 12:27:53 +0900

    add : 자료구조 - CPU 스케줄링 (주석 수정)

--- algorithm/src/inflearn/data_structure/SJFScheduling.java
+++ algorithm/src/inflearn/data_structure/SJFScheduling.java
@@ -2,13 +2,13 @@ package inflearn.data_structure;
 import java.util.*;
 public class SJFScheduling {
     public int[] solution(int[][] tasks) {
-        int n = tasks.length, endTime = 0, order = 0;
-        ArrayList<Integer> result = new ArrayList<>();
+        int n = tasks.length, endTime = 0, idx = 0;
+        int[] answer = new int[n];
         LinkedList<int[]> programs = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             programs.add(new int[]{tasks[i][0], tasks[i][1], i}); // 도착시간, 작업시간, 작업 번호
         }
-        // 호출 시간에서 오름차순 정렬
+        // 호출 시간에 따라 오름차순 정렬
         programs.sort((a, b)-> a[0] - b[0]);
         // 도착시간이 같다면, 작업시간 기준으로 정렬
         PriorityQueue<int[]> readyQ = new PriorityQueue<>((a, b)-> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
@@ -16,16 +16,13 @@ public class SJFScheduling {
             if(readyQ.isEmpty()) endTime = Math.max(endTime, programs.peek()[0]); // 도착시간
             // 작업해야 할 프로그램이 남아있고, 도착시간이 현재 작업이 끝날 시간보다 작거나 같다면
             while(!programs.isEmpty() && programs.peek()[0] <= endTime) {
-                int[] addQueue = programs.pollFirst(); // 아마도 값을 넣어주는 듯?
+                int[] addQueue = programs.pollFirst(); // 링크드 리스트의 제일 앞의 자료를 꺼내는 메소드
                 readyQ.add(new int[]{addQueue[1], addQueue[2]}); // 작업 시간, 작업 번호
             }
-            int[] processing = readyQ.poll(); // 처리할 작업
-            result.add(processing[1]); // 결과에 현재 작업 번호 추가
+            int[] processing = readyQ.poll(); // cpu가 현재 처리할 작업
+            answer[idx++] = processing[1]; // 현재 작업 번호를 처리 순서대로 저장
             endTime = endTime + processing[0]; // 이 작업이 작업이 끝나는 시간 업데이트
         }
-        int[] answer = new int[n];
-        for(int i = 0; i < n; i++) answer[i] = result.get(i);
-        
         return answer;
     }
 
