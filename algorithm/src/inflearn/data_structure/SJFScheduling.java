5d13d4546750bc93e2475cc6499cd813c4e4e4da
wjdtk <wjdtkdgml7352@naver.com>
2025-01-05 12:17:14 +0900

    add : 자료구조 - CPU 스케줄링 (리팩토링)

--- algorithm/src/inflearn/data_structure/SJFScheduling.java
+++ algorithm/src/inflearn/data_structure/SJFScheduling.java
@@ -1,42 +1,32 @@
 package inflearn.data_structure;
 import java.util.*;
 public class SJFScheduling {
-    public int[] solution(int[][] tasks){
+    public int[] solution(int[][] tasks) {
         int n = tasks.length, endTime = 0, order = 0;
-        int[] answer = new int[n];
-        PriorityQueue<Task> readyQ = new PriorityQueue<>(new Comparator<Task>() {
-            @Override
-            public int compare(Task o1, Task o2) {
-                return o1.taskTime - o2.taskTime;
-            }
-        });
-        ArrayList<Task> sort = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        LinkedList<int[]> programs = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            sort.add(new Task(i, tasks[i][0], tasks[i][1]));
+            programs.add(new int[]{tasks[i][0], tasks[i][1], i}); // 도착시간, 작업시간, 작업 번호
         }
-        Collections.sort(sort);
-        Task first = sort.get(0);
-        readyQ.offer(first);
-        endTime = first.arrivalTime; // 초기화
-        for (int i = 1, t = endTime; ; t++) { // i는 arrayList의 배열 인덱스
-            // 도착시간이 작업이 끝나기 전까지의 시간보다 작거나 같다면 큐에 저장
-            while(i < n && sort.get(i).arrivalTime <= endTime) {
-                readyQ.offer(sort.get(i));
-                i++;
+        // 호출 시간에서 오름차순 정렬
+        programs.sort((a, b)-> a[0] - b[0]);
+        // 도착시간이 같다면, 작업시간 기준으로 정렬
+        PriorityQueue<int[]> readyQ = new PriorityQueue<>((a, b)-> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        while(!programs.isEmpty() || !readyQ.isEmpty()) {
+            if(readyQ.isEmpty()) endTime = Math.max(endTime, programs.peek()[0]); // 도착시간
+            // 작업해야 할 프로그램이 남아있고, 도착시간이 현재 작업이 끝날 시간보다 작거나 같다면
+            while(!programs.isEmpty() && programs.peek()[0] <= endTime) {
+                int[] addQueue = programs.pollFirst(); // 아마도 값을 넣어주는 듯?
+                readyQ.add(new int[]{addQueue[1], addQueue[2]}); // 작업 시간, 작업 번호
             }
-            if(endTime == t && !readyQ.isEmpty()){
-                // answer에 저장
-                Task cur = readyQ.poll();
-                answer[order++] = cur.num; // order은 작업 순서 인덱스
-                endTime = endTime + cur.taskTime;
-            }
-            if(readyQ.isEmpty() && endTime < sort.get(i).arrivalTime) {
-                t = endTime = sort.get(i).arrivalTime;
-                readyQ.offer(sort.get(i++));
-            }
-
-            if(order == n - 1) return answer;
+            int[] processing = readyQ.poll(); // 처리할 작업
+            result.add(processing[1]); // 결과에 현재 작업 번호 추가
+            endTime = endTime + processing[0]; // 이 작업이 작업이 끝나는 시간 업데이트
         }
+        int[] answer = new int[n];
+        for(int i = 0; i < n; i++) answer[i] = result.get(i);
+        
+        return answer;
     }
 
     public static void main(String[] args){
@@ -45,19 +35,5 @@ public class SJFScheduling {
         System.out.println(Arrays.toString(T.solution(new int[][]{{5, 2}, {7, 3}, {1, 3}, {1, 5}, {2, 2}, {1, 1}})));
         System.out.println(Arrays.toString(T.solution(new int[][]{{1, 2}, {2, 3}, {1, 3}, {3, 3}, {8, 2}, {1, 5}, {2, 2}, {1, 1}})));
         System.out.println(Arrays.toString(T.solution(new int[][]{{999, 1000}, {996, 1000}, {998, 1000}, {999, 7}})));
-    }
-}
-class Task implements Comparable<Task> {
-    int num;
-    int arrivalTime;
-    int taskTime;
-    public Task(int num,int arrivalTime, int taskTime) {
-        this.num = num;
-        this.arrivalTime = arrivalTime;
-        this.taskTime = taskTime;
-    }
-    @Override
-    public int compareTo(Task o) {
-        return this.arrivalTime - o.arrivalTime;
     }
 }
