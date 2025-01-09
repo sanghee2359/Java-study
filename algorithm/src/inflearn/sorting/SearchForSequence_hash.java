a83f3d2cd0f28566312b335e9d6c85f5fcac0289
wjdtk <wjdtkdgml7352@naver.com>
2025-01-09 12:34:50 +0900

    add : 정렬 - 수열 찾기 (리팩토링)

--- algorithm/src/inflearn/sorting/SearchForSequence_hash.java
+++ algorithm/src/inflearn/sorting/SearchForSequence_hash.java
@@ -1,20 +1,23 @@
 package inflearn.sorting;
 import java.util.*;
-public class SearchForSequence {
-    // ArrayList 자료구조 활용
+public class SearchForSequence_hash {
+    // HashMap 자료구조 활용 (수열 안에 중복되는 숫자도 함께 관리)
     public int[] solution(int[] nums){
         long startTime = System.nanoTime();
         int[] answer = new int[nums.length / 2];
-        ArrayList<Integer> numList = new ArrayList<>();
-        for(int x : nums) {
-            numList.add(x);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(nums); // 오름차순 정렬
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
         int idx = 0;
-        for(int x : numList){
-            if(!numList.contains(x * 2)) continue;
+        for (int x : nums) {
+            if(map.get(x) == 0) continue;
             answer[idx++] = x;
+            map.put(x, map.get(x) - 1);
+            map.put(x * 2, map.get(x * 2) - 1);
+
         }
-        Arrays.sort(answer);
         long endTime = System.nanoTime(); // 끝 시간 측정
         System.out.println("Execution Time: " + (endTime - startTime) + " ns");
 
@@ -22,7 +25,7 @@ public class SearchForSequence {
     }
 
     public static void main(String[] args){
-        SearchForSequence T = new SearchForSequence();
+        SearchForSequence_hash T = new SearchForSequence_hash();
         System.out.println(Arrays.toString(T.solution(new int[]{1, 10, 2, 3, 5, 6})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 1, 6, 2, 2, 7, 3, 14})));
         System.out.println(Arrays.toString(T.solution(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14})));
