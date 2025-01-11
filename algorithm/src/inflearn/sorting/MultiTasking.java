6514e01579c11a07846a44ad6f0a2de496de058e
wjdtk <wjdtkdgml7352@naver.com>
2025-01-11 13:04:49 +0900

    add : 정렬 - 멀티태스킹 (리팩토링)

--- algorithm/src/inflearn/sorting/MultiTasking.java
+++ algorithm/src/inflearn/sorting/MultiTasking.java
@@ -7,30 +7,24 @@ public class MultiTasking {
         int answer = 0;
         int res = tasks.length;
         int[] sT = new int[res+1];
-        for (int i = 1; i < sT.length; i++) {
-            sT[i] = tasks[i - 1];
-        }
+        System.arraycopy(tasks, 0, sT, 1, tasks.length);
         Arrays.sort(sT);
         // st에서 i를 통해 반복문 시작
         for (int i = 1; i < sT.length; i++) {
-            int repeat = sT[i] - sT[i-1];
-            if(k - (long) res * repeat >= 0) {
-                k = k - (long) res * repeat;
-                res--; // 남은 작업 1 제외
-                continue;
-            }
-            // 이제 3이 남았음 -> sT가 아닌, 실제 작업스케쥴러에서 1씩 시뮬레이션
-            int idx = 0; // 작업스케쥴러 0번인덱스부터 작업 시뮬 시작
-            while(k>0) {
-                if(sT[i]> tasks[idx]) {
-                    idx = (idx + 1) % tasks.length;
-                    continue;
+            long time = ((long) res *(sT[i] - sT[i-1]));
+            if(k < time) {
+                long idx = k % res; // 남은 작업시간을 남은 작업들의 나머지로 구함
+                int count = 0;
+                for (int j = 0; j < tasks.length; j++) {
+                    if(tasks[j] >= sT[i]) { // 아직 작업이 남은 것만
+                        if(count == idx) return j+1;
+                        count++; // 남은 k시간 만큼 작업 수행
+                    }
                 }
-                k--;
-                idx = (idx+1) % tasks.length; // 다음 인덱스로 이동
+            }else {
+                k = k - time;
+                res--;
             }
-            return idx + 1;
-
         }
         return -1;
     }
