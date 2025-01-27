d51316154258d58ec925d7c7c0afae7cc75d941b
wjdtk <wjdtkdgml7352@naver.com>
2025-01-27 10:52:12 +0900

    add : 그리디 - 스프링 쿨러 (리팩토링)

--- algorithm/src/inflearn/greedy/SpringCooler.java
+++ algorithm/src/inflearn/greedy/SpringCooler.java
@@ -1,22 +1,26 @@
 package inflearn.greedy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class SpringCooler {
     public int solution(int n, int[] nums){
         int answer = 0;
-        List<Point> spot = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            spot.add(new Point(nums[i], i));
+        int[][] line = new int[n+1][2]; // 스프링 쿨러는 n+1개
+        for (int i = 0; i < n+1; i++) {
+            line[i][0] = Math.max((i - nums[i]), 0);
+            line[i][1] = Math.min((i + nums[i]), n); // 최대 범위를 넘어가지 않도록
         }
-        Collections.sort(spot);
-
-        for () {
-            Point point = spot.get(0);
-
+        Arrays.sort(line, (a, b)-> a[0] - b[0]); // 시작값 기준 오름차순
+        int start = 0, end = 0, i =0;
+        while(i < line.length){
+            while(i < line.length && line[i][0] <= start) {
+                end = Math.max(line[i][1], end); 
+                i++; // start 지점부터 가장 길게 물을 뿌리는 스프링 쿨러를 찾는 while문
+            }
+            answer++;
+            if(end == n) return answer; // 물을 다 뿌림
+            if(start == end) return -1;
+            start = end;
         }
         return answer;
     }
@@ -29,19 +33,4 @@ public class SpringCooler {
         System.out.println(T.solution(11, new int[]{1, 2, 3, 1, 2, 1, 1, 2, 1, 1, 1, 1}));
     }
 }
-class Point implements Comparable<Point> {
 
-    int start;
-    int end;
-    int range;
-    public Point(int num, int idx) {
-        this.start = idx - num;
-        this.end = idx + num;
-        this.range = end - start;
-    }
-    @Override
-    public int compareTo(Point o) {
-        if(this.range == o.range) return this.end - o.end;
-        return this.range - o.range;
-    }
-}
